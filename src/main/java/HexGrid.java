4e36391b2323e708894de7d1053dd953d0214d01
Anseroidea <K1525058@students.katyisd.rog>
2022-04-06 08:06:03 -0500

    spiral matrix

--- src/main/java/HexGrid.java
+++ src/main/java/HexGrid.java
@@ -1,32 +1,63 @@
 import java.util.*;
 import java.util.stream.IntStream;
 
-public class HexGrid<T> {
+public class HexGrid {
 
-    private Map<Integer, List<T>> map;
+    private Map<Integer, List<Tile>> map;
     private VertexManager vertexManager;
 
-    public <T> HexGrid(){
+    public HexGrid(){
          map = new TreeMap<>();
          vertexManager = new VertexManager();
     }
 
-    public void add(T t, int r, int c){
+    public void add(Tile t, int r, int c){
         if (c < 0){
             return;
         }
         if (!map.containsKey(r)){
-            List<T> list = new ArrayList<>();
+            List<Tile> list = new ArrayList<>();
             IntStream.range(0, c).forEach(i -> list.add(null));
             list.add(t);
             map.put(r, list);
         } else {
-            List<T> list = map.get(r);
+            List<Tile> list = map.get(r);
             if (list.size() > c){
                 list.set(c, t);
             } else {
                 IntStream.range(list.size(), c).forEach(i -> list.add(null));
                 list.add(t);
+            }
+        }
+        t.setPos(r, c);
+    }
+
+    public void setAdjacencies(){
+        for (Integer r : map.keySet()){
+            for (int c = 0; c < map.get(r).size(); c++) {
+                Tile t = map.get(r).get(c);
+                if (map.containsKey(r - 1)){
+                    if (map.get(r - 1).size() > c - 1 && c > 0) {
+                        t.setNearbyTile(Tile.SOUTHWEST, map.get(r - 1).get(c - 1));
+                    }
+                    if (map.get(r - 1).size() > c + 1) {
+                        t.setNearbyTile(Tile.SOUTHEAST, map.get(r - 1).get(c + 1));
+                    }
+                }
+                if (map.containsKey(r + 1)){
+                    if (map.get(r + 1).size() > c - 1 && c > 0) {
+                        t.setNearbyTile(Tile.NORTHWEST, map.get(r + 1).get(c - 1));
+                    }
+                    if (map.get(r + 1).size() > c + 1) {
+                        t.setNearbyTile(Tile.NORTHEAST, map.get(r + 1).get(c + 1));
+                    }
+                }
+                if (map.get(r).size() > c + 1){
+                    t.setNearbyTile(Tile.EAST, map.get(r).get(c));
+                }
+                if (c > 0){
+                    t.setNearbyTile(Tile.WEST, map.get(r).get(c - 1));
+                }
             }
         }
     }
@@ -39,7 +70,12 @@ public class HexGrid<T> {
         return map.containsKey(r) && c < map.get(r).size() && map.get(r).get(c) != null;
     }
 
-    protected Map<Integer, List<T>> getMap(){
+    protected Map<Integer, List<Tile>> getMap(){
         return map;
     }
+
+    public Tile get(int r, int c){
+        return map.get(r).get(c);
+    }
+
 }
