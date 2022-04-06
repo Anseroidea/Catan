8b6f81ac470d01c5bef572deceb26c2d19c4d736
slhscs203 <sadhanaabala@gmail.com>
2022-04-06 07:29:54 -0500

    vertex class

--- src/main/java/Edge.java
+++ src/main/java/Edge.java
@@ -1,2 +1,20 @@
+import java.util.Map;
+
 public class Edge {
+    public Map<Integer, Tile> adjacentTiles;
+    public Map<Integer, Vertex> adjacentVertices;
+    public Road road;
+
+    public Edge() {
+    }
+
+    public Map<Integer, Tile> getAdjacentTiles() {
+        return adjacentTiles;
+    }
+
+    public Map<Integer, Vertex> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+
+
 }
