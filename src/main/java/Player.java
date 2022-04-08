93385fc06d584ffcce6b15696b9074e03a164d16
slhscs203 <sadhanaabala@gmail.com>
2022-04-08 08:05:47 -0500

    vertex class

---
+++ src/main/java/Player.java
@@ -0,0 +1,12 @@
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.*;
+
+public class Player {
+    private String name;
+    private Map<Resource, Integer> resources;
+}
