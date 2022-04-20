66a989ee42968cfa3444a03a373426ac41a084b2
lektphire <liuant@protonmail.com>
2022-04-20 08:05:24 -0500

    Dc

--- src/main/java/PopUp.java
+++ src/main/java/PopUp.java
@@ -1,13 +1,53 @@
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
 import java.util.List;
 
 public enum PopUp {
     DICE, TRADEBANK, TRADE, CONVERT, MONOPOLY, ROAD;
 
-    public static void load() {
+    private Pane pane;
+
+    private Object controller;
+
+    private static PopUp cur;
+
+    public void setController(Object c)
+    {
+        controller = c;
+    }
+
+    public void setPane(Pane a)
+    {
+        pane = a;
+    }
+
+    public Pane getPane()
+    {
+        return pane;
+    }
+
+    public void load()
+    {
+        Stage pop = new Stage();
+        pop.initModality(Modality.APPLICATION_MODAL);
+        pop.setScene(new Scene(pane, 1920, 1080));
+        pop.setResizable(false);
 
     }
 
-    public static void loadTrade(Player p, List<Resource> l, Player pl, List<Resource> lr) {
-
+    public void loadTrade(Player p, List<Resource> l, Player pl, List<Resource> lr)
+    {
+        if(this != TRADE)
+        {
+            System.out.println("This is not player trade");
+            return;
+        }
+        Stage pop = new Stage();
+        pop.initModality(Modality.APPLICATION_MODAL);
+        pop.setScene(new Scene(pane, 1920, 1080));
+        pop.setResizable(false);
     }
 }
