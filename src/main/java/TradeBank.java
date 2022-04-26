1377f502af1fd3212806689e747fee6b379091d0
lektphire <liuant@protonmail.com>
2022-04-26 07:46:10 -0500

    Dc

--- src/main/java/TradeBank.java
+++ src/main/java/TradeBank.java
@@ -13,6 +13,7 @@ import javafx.stage.Stage;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Objects;
 
@@ -22,9 +23,9 @@ public class TradeBank
     private Button confirmTrade, changeRequest;
 
     @FXML
-    private StackPane a, b, c, d, e;
+    private StackPane a, b, c, d, e, a1, b1, c1, d1, e1;
 
-    private String give, get;
+    private String give, get, givePane, getPane;
 
     public static Map<String, Image> cardGraphics;
 
@@ -55,8 +56,14 @@ public class TradeBank
         ((ImageView)c.getChildren().get(0)).setImage(cardGraphics.get("forest"));
         ((ImageView)d.getChildren().get(0)).setImage(cardGraphics.get("ore"));
         ((ImageView)e.getChildren().get(0)).setImage(cardGraphics.get("sheep"));
+        ((ImageView)a1.getChildren().get(0)).setImage(cardGraphics.get("brick"));
+        ((ImageView)b1.getChildren().get(0)).setImage(cardGraphics.get("wheat"));
+        ((ImageView)c1.getChildren().get(0)).setImage(cardGraphics.get("forest"));
+        ((ImageView)d1.getChildren().get(0)).setImage(cardGraphics.get("ore"));
+        ((ImageView)e1.getChildren().get(0)).setImage(cardGraphics.get("sheep"));
         //System.out.println(5);
     }
+
 
     public void confirmBankTrade(MouseEvent mouseEvent)
     {
@@ -70,9 +77,15 @@ public class TradeBank
         Map<Resource, Integer> res = p.getResources();
 
         Resource loss = new Resource(give);
-        if(res.get(loss) < 4)
+        if(get.equals(give))
         {
             Alert a = new Alert(Alert.AlertType.INFORMATION);
+            a.setContentText("Cannot give and get the same resource!");
+            a.show();
+        }
+        else if(res.get(loss) < 4)
+        {
+            Alert a = new Alert(Alert.AlertType.WARNING);
             a.setContentText("Not enough cards!");
             a.show();
         }
@@ -81,26 +94,37 @@ public class TradeBank
             res.put(loss, res.get(loss) - 4);
             Resource gain = new Resource(get);
             res.put(gain, res.get(gain) + 1);
+
+            for(Iterator<Map.Entry<Resource, Integer>> e = res.entrySet().iterator(); e.hasNext();)
+            {
+                Map.Entry<Resource, Integer> temp = e.next();
+                System.out.println(temp.getKey().getResource() + " " + temp.getValue());
+            }
+            give = get = givePane = getPane = " ";
             ((Stage)((Button)mouseEvent.getSource()).getScene().getWindow()).close();
         }
     }
 
     public void addBorder(MouseEvent mouseEvent)
     {
-        ((Pane)mouseEvent.getSource()).setStyle("-fx-border-color: black");
+        Pane clicked = ((Pane)mouseEvent.getSource());
+        if(!(clicked.getId().equals(getPane) || clicked.getId().equals(givePane)))
+            ((Pane)mouseEvent.getSource()).setStyle("-fx-border-color: black");
     }
 
     public void removeBorder(MouseEvent mouseEvent)
     {
-        ((Pane)mouseEvent.getSource()).setStyle("-fx-border-color: transparent");
+        Pane clicked = ((Pane)mouseEvent.getSource());
+        if(!(clicked.getId().equals(getPane) || clicked.getId().equals(givePane)))
+            ((Pane)mouseEvent.getSource()).setStyle("-fx-border-color: transparent");
     }
 
     public void select(MouseEvent mouseEvent)
     {
-        clearBorders();
+        clearGiveBorders();
         Pane chosen = (Pane)mouseEvent.getSource();
         chosen.setStyle("-fx-border-color: blue");
-        get = switch(chosen.getId())
+        give = switch(chosen.getId())
         {
             case "a" -> "brick";
             case "b" -> "wheat";
@@ -109,15 +133,43 @@ public class TradeBank
             case "e" -> "sheep";
             default -> "Nothing";
         };
-        System.out.println(get);
+        givePane = chosen.getId();
+        System.out.println(give);
     }
 
-    private void clearBorders()
+    private void clearGiveBorders()
     {
         a.setStyle("-fx-border-color: transparent");
         b.setStyle("-fx-border-color: transparent");
         c.setStyle("-fx-border-color: transparent");
         d.setStyle("-fx-border-color: transparent");
         e.setStyle("-fx-border-color: transparent");
+    }
+
+    public void choose(MouseEvent mouseEvent)
+    {
+        clearGetBorders();
+        Pane chosen = (Pane)mouseEvent.getSource();
+        chosen.setStyle("-fx-border-color: blue");
+        get = switch(chosen.getId())
+                {
+                    case "a1" -> "brick";
+                    case "b1" -> "wheat";
+                    case "c1" -> "forest";
+                    case "d1" -> "ore";
+                    case "e1" -> "sheep";
+                    default -> "Nothing";
+                };
+        getPane = chosen.getId();
+        System.out.println(get);
+    }
+
+    private void clearGetBorders()
+    {
+        a1.setStyle("-fx-border-color: transparent");
+        b1.setStyle("-fx-border-color: transparent");
+        c1.setStyle("-fx-border-color: transparent");
+        d1.setStyle("-fx-border-color: transparent");
+        e1.setStyle("-fx-border-color: transparent");
     }
 }
