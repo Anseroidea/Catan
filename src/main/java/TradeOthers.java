437fe496e4df5aa83ee6ed384d95e8879d7aec51
lektphire <93264392+lektphire@users.noreply.github.com>
2022-05-03 08:05:54 -0500

    Update TradeOthers.java

--- src/main/java/TradeOthers.java
+++ src/main/java/TradeOthers.java
@@ -1,12 +1,22 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class TradeOthers
 {
     @FXML
     private Button oneA, oneD, twoA, twoD, threeA, threeD, one, two, three;
+
+    @FXML
+    private HBox request, offer;
 
     private HashMap<String, Integer> get, give;
 
@@ -18,5 +28,21 @@ public class TradeOthers
     public void setGive(HashMap<String, Integer> g)
     {
         give = g;
+    }
+
+    //Graphics, enable / disable buttons
+    public void setBasics()
+    {
+        for(Iterator<Map.Entry<String, Integer>> i = get.entrySet().iterator(); i.hasNext();)
+        {
+            Map.Entry<String, Integer> e = i.next();
+            VBox v = new VBox();
+            v.getChildren().add(new ImageView(TradeBank.cardGraphics.get(e.getKey())));
+            Label amount = new Label();
+            amount.setText("" + e.getValue());
+            v.getChildren().add(amount);
+            v.setVisible(true);
+            request.getChildren().add(v);
+        }
     }
 }
