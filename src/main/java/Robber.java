8753c0e683ee4af6a6a7f736b5e86ce85d0b8ce7
JamesZJGuo <jameszijianguo@gmail.com>
2022-05-06 07:44:30 -0500

    Robber now combination of previous two ideas: separate class and boolean value. Graphics of robber added.

--- src/main/java/Robber.java
+++ src/main/java/Robber.java
@@ -1,7 +1,39 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
-import javafx.scene.*;
 
 public class Robber {
     Tile t;
-
+    ArrayList<Player> p;
+    public Robber(List<Player> pa){
+        p= (ArrayList<Player>) pa;
+    }
+    public void move7(Tile j){
+        t=j;
+        t.robber=true;
+        for(Player a:p){
+            if(a.getResources().size()>7){
+                ArrayList<Resource> y=(ArrayList<Resource>) a.getResources();
+                for(int t=0;t<y.size()/2;t++){
+                    int o=(int)Math.random()*y.size()+1;
+                    Resource m=y.remove(o);
+                    a.changeCards(m,-1);
+                }
+            }
+        }
+    }
+    public void move(Tile w){
+        t=w;
+        t.robber=true;
+    }
+    public BufferedImage getGraphics(){
+        try {
+            return ImageIO.read(new File("robber.png"));
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+        return null;
+    }
 }
